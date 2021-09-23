8bd56b540b9791d2aca59cfea0e9c3ce3cf59b3a
haz/ahme <hazem.belhadj222@gmail.com>
2021-09-23 09:50:27 +0100

    add log

--- app/src/main/java/com/example/hazem/MainActivity.java
+++ app/src/main/java/com/example/hazem/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -22,10 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity" , "add log");
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
